ac83619a964780c51188fea3389a720e945aa449
joseluizmed <86578170+joseluizmed@users.noreply.github.com>
2025-07-24 17:54:11 -0300

    teste com giskus

--- components/GiscusComponent.tsx
+++ components/GiscusComponent.tsx
@@ -20,18 +20,18 @@ const GiscusComponent: React.FC = () => {
     // --- IMPORTANTE: CONFIGURE SEU REPOSITÓRIO GISCUS AQUI ---
     // Você pode obter esses valores na página de configuração do Giscus: https://giscus.app
     // Substitua os valores de exemplo pelos seus.
-    script.setAttribute("data-repo", "josesneto/tempo-de-atestado"); 
-    script.setAttribute("data-repo-id", "R_kgDOMGV42w");
+    script.setAttribute("data-repo", "joseluizmed/tempodeatestadonovo2"); 
+    script.setAttribute("data-repo-id", "R_kgDOPP5azg");
     script.setAttribute("data-category", "Announcements");
-    script.setAttribute("data-category-id", "DIC_kwDOMGV4284Chz8Q");
+    script.setAttribute("data-category-id", "DIC_kwDOPP5azs4CtPj3");
     // --- FIM DA CONFIGURAÇÃO ---
 
-    script.setAttribute("data-mapping", "pathname");
-    script.setAttribute("data-strict", "0");
+    script.setAttribute("data-mapping", "url");
+    script.setAttribute("data-strict", "1");
     script.setAttribute("data-reactions-enabled", "1");
     script.setAttribute("data-emit-metadata", "0");
-    script.setAttribute("data-input-position", "top");
-    script.setAttribute("data-theme", "light"); // Opções: 'light', 'dark', 'preferred_color_scheme'
+    script.setAttribute("data-input-position", "bottom");
+    script.setAttribute("data-theme", "preferred_color_scheme");
     script.setAttribute("data-lang", "pt");
     script.setAttribute("crossorigin", "anonymous");
     script.async = true;
